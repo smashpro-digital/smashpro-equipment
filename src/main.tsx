41c4d0fa953f498d7963579d7a90022a2000ab94
Maurice Jordan <mauriceajordan@gmail.com>
2026-09-03 07:25:20 -0400

    fix(journey): keep days since production calendar-accurate

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
+import { installJourneyCalendarMetricCorrection } from "./domain/calendarMetrics";
 import "./styles/global.css";
 import "./styles/dream-build-fixes.css";
 import "./styles/catalog.css";
 import "./styles/product-engineering.css";
+
+installJourneyCalendarMetricCorrection();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
