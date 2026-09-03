37e22a44caccf83aa095066af618b469d9445090
Maurice Jordan <mauriceajordan@gmail.com>
2026-09-03 07:53:14 -0400

    feat(history): install negotiation evidence media

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app/App";
+import { installNegotiationEvidenceMedia } from "./app/negotiationEvidenceMedia";
 import { installJourneyCalendarMetricCorrection } from "./domain/calendarMetrics";
 import "./styles/global.css";
 import "./styles/dream-build-fixes.css";
 import "./styles/catalog.css";
 import "./styles/product-engineering.css";
+import "./styles/manufacturer-profile.css";
+import "./styles/negotiation-evidence-media.css";
 
 installJourneyCalendarMetricCorrection();
+installNegotiationEvidenceMedia();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
